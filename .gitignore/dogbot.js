d184cd66cc4e18758967b428dfc5a2d18dd72935
Nefheure <43937013+Nefheure@users.noreply.github.com>
2018-11-10 21:37:46 +0100

    Update dogbot.js

--- .gitignore/dogbot.js
+++ .gitignore/dogbot.js
@@ -26,11 +26,20 @@ bot.on('message', message => {
     if(message.content === prefix + "youtube"){
         var youtube_embed = new Discord.RichEmbed()
         .setColor("#000000")
-        .setTitle("Voici la chaine Youtube de **DogSterFX** si vous avez manquer une video courer la voir ! ")
+        .setTitle("Voici la chaine Youtube de **DogSterFX** si vous avez manquer une video courrer la voir ! ")
         .addField("*lien :*", "https://www.youtube.com/channel/UCUm2bH23aU5SdqZfKX5nE_g")
         message.channel.sendMessage(youtube_embed);
     }
 });
 
-bot.login(process.env.TOKEN);
+bot.on('message', message => {
 
+    if(message.content === prefix + "gfx"){
+        var gfx_embed = new Discord.RichEmbed()
+        .setColor("#000000")
+        .setTitle("Si vous voulez demander Une Banniere, Un Logo ou autre a **DogSterFX** tous se passe dans le lien ci-dessous !")
+        .addField("*lien :*", "https://goo.gl/forms/JY0777g2zWOoGQR33")
+    }
+});
+
+bot.login(process.env.TOKEN);
